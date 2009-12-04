ca07ca8d62a1379f9279df2cf94c25c78b2644c4
Samuel Rivas <samuel.rivas@lambdastream.com>
2009-12-04 14:27:11 +0100

    TEST transform test0 in a EUnit-fixture test
    It is failing though, there is a hidden call to former mock module in lstd_mock
    implementation

--- ltest_mock_eunit.erl
+++ ltest_mock_eunit.erl
@@ -11,7 +11,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 suite() ->
-    test0_test(),
+%    test0_test(),
     test0a_test(),
     test0b_test(),
     test0c_test(),
@@ -31,94 +31,104 @@ suite() ->
     test7a_test(),
     io:format("~n~nfinished without unexpected errors! error reports may be ignored!!~n~n~n").
 
-test0_test() ->
-    Mock = mock:new(),
-    mock:expect(Mock, in_order, testmodule1, mockme1, [1,2], {return, ok}),
-    mock:expect(Mock, in_order, testmodule1, mockme2, [2,3], {return, ok}),
-    mock:expect(Mock, in_order, testmodule2, mockme2, [3,2], {return, ok}),
-    mock:expect(Mock, in_order, testmodule2, mockme1, [1,2], {return, ok}),
-
-    mock:replay(Mock),
-
-    testmodule1:mockme1(1,2),
-    testmodule1:mockme2(2,3),
-    testmodule2:mockme2(3,2),
-    testmodule2:mockme1(1,2),
-
-    mock:verify(Mock).
+test0_test_() ->
+    {setup,
+     fun () ->
+             Mock = ltest_mock:new(),
+             ltest_mock:expect(
+               Mock, in_order, testmodule1, mockme1, [1,2], {return, ok}),
+             ltest_mock:expect(
+               Mock, in_order, testmodule1, mockme2, [2,3], {return, ok}),
+             ltest_mock:expect(
+               Mock, in_order, testmodule2, mockme2, [3,2], {return, ok}),
+             ltest_mock:expect(
+               Mock, in_order, testmodule2, mockme1, [1,2], {return, ok}),
+             ltest_mock:replay(Mock),
+             Mock
+     end,
+     fun (_) -> ok end,
+     fun (Mock) ->
+             [
+              ?_assertEqual(ok, testmodule1:mockme1(1,2)),
+              ?_assertEqual(ok, testmodule1:mockme2(2,3)),
+              ?_assertEqual(ok, testmodule2:mockme2(3,2)),
+              ?_assertEqual(ok, testmodule2:mockme1(1,2)),
+              ?_test(ltest_mock:verify(Mock))
+             ]
+     end}.
 
 test0a_test() ->
-    Mock = mock:new(),
-    mock:expect(Mock, in_order, testmodule1, mockme1, [1,2], {return, ok}),
-    mock:expect(Mock, in_order, testmodule1, mockme2, [2,3], {return, ok}),
-    mock:expect(Mock, in_order, testmodule2, mockme2, [3,2], {return, ok}),
-    mock:expect(Mock, in_order, testmodule2, mockme1, [1,2], {return, ok}),
+    Mock = ltest_mock:new(),
+    ltest_mock:expect(Mock, in_order, testmodule1, mockme1, [1,2], {return, ok}),
+    ltest_mock:expect(Mock, in_order, testmodule1, mockme2, [2,3], {return, ok}),
+    ltest_mock:expect(Mock, in_order, testmodule2, mockme2, [3,2], {return, ok}),
+    ltest_mock:expect(Mock, in_order, testmodule2, mockme1, [1,2], {return, ok}),
 
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
 
     testmodule1:mockme1(1,2),
     testmodule1:mockme2(2,3),
     {'EXIT',_} = (catch testmodule2:mockme1(1,2)).
 
 test0b_test() ->
-    Mock = mock:new(),
-    mock:expect(Mock, in_order, testmodule1, mockme1, [1,2], {return, ok}),
-    mock:expect(Mock, in_order, testmodule1, mockme2, [2,3], {return, ok}),
-    mock:expect(Mock, in_order, testmodule2, mockme2, [3,2], {return, ok}),
-    mock:expect(Mock, in_order, testmodule2, mockme1, [1,2], {return, ok}),
+    Mock = ltest_mock:new(),
+    ltest_mock:expect(Mock, in_order, testmodule1, mockme1, [1,2], {return, ok}),
+    ltest_mock:expect(Mock, in_order, testmodule1, mockme2, [2,3], {return, ok}),
+    ltest_mock:expect(Mock, in_order, testmodule2, mockme2, [3,2], {return, ok}),
+    ltest_mock:expect(Mock, in_order, testmodule2, mockme1, [1,2], {return, ok}),
 
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
 
     testmodule1:mockme1(1,2),
     testmodule1:mockme2(2,3),
     testmodule2:mockme2(3,2),
 
-    {error, _} = mock:verify(Mock).
+    {error, _} = ltest_mock:verify(Mock).
 
 test0c_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule1, mockme1, [1,2], {return, ok}),
-    mock:o_o   (Mock, testmodule1, mockme2, [2,3], {return, ok}),
-    mock:strict(Mock, testmodule2, mockme2, [3,2], {return, ok}),
-    mock:o_o   (Mock, testmodule2, mockme1, [1,2], {return, ok}),
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule1, mockme1, [1,2], {return, ok}),
+    ltest_mock:o_o   (Mock, testmodule1, mockme2, [2,3], {return, ok}),
+    ltest_mock:strict(Mock, testmodule2, mockme2, [3,2], {return, ok}),
+    ltest_mock:o_o   (Mock, testmodule2, mockme1, [1,2], {return, ok}),
 
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
 
     testmodule1:mockme1(1,2),
     testmodule2:mockme2(3,2),
     testmodule2:mockme1(1,2),
     testmodule1:mockme2(2,3),
 
-    mock:verify(Mock).
+    ltest_mock:verify(Mock).
 
 test0d_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule1, mockme1, [1,2], {return, ok}),
-    mock:o_o   (Mock, testmodule1, mockme2, [2,3], {return, ok}),
-    mock:strict(Mock, testmodule2, mockme2, [3,2], {return, ok}),
-    mock:o_o   (Mock, testmodule2, mockme1, [1,2], {return, ok}),
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule1, mockme1, [1,2], {return, ok}),
+    ltest_mock:o_o   (Mock, testmodule1, mockme2, [2,3], {return, ok}),
+    ltest_mock:strict(Mock, testmodule2, mockme2, [3,2], {return, ok}),
+    ltest_mock:o_o   (Mock, testmodule2, mockme1, [1,2], {return, ok}),
 
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
 
     testmodule1:mockme1(1,2),
     testmodule2:mockme2(3,2),
     testmodule2:mockme1(1,2),
 
-    {error, _} = mock:verify(Mock).
+    {error, _} = ltest_mock:verify(Mock).
 
 test0e_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule1, mockme1, [1,2], {return, ok}),
-    mock:o_o   (Mock, testmodule1, mockme2, [2,3], {return, ok}),
-    mock:strict(Mock, testmodule2, mockme2, [3,2], {return, ok}),
-    mock:o_o   (Mock, testmodule2, mockme1, [1,2], {return, ok}),
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule1, mockme1, [1,2], {return, ok}),
+    ltest_mock:o_o   (Mock, testmodule1, mockme2, [2,3], {return, ok}),
+    ltest_mock:strict(Mock, testmodule2, mockme2, [3,2], {return, ok}),
+    ltest_mock:o_o   (Mock, testmodule2, mockme1, [1,2], {return, ok}),
 
-    mock:stub  (Mock, testmodule3, mockme, [666], {return, ok}),
-    mock:stub  (Mock, testmodule3, mockme, [777], {return, ok}),
-    mock:stub  (Mock, testmodule3, mockme, [888], {return, ok}),
-    mock:stub  (Mock, testmodule3, mockme, [999], {return, ok}),
+    ltest_mock:stub  (Mock, testmodule3, mockme, [666], {return, ok}),
+    ltest_mock:stub  (Mock, testmodule3, mockme, [777], {return, ok}),
+    ltest_mock:stub  (Mock, testmodule3, mockme, [888], {return, ok}),
+    ltest_mock:stub  (Mock, testmodule3, mockme, [999], {return, ok}),
 
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
 
     ok = testmodule1:mockme1(1,2),
     ok = testmodule2:mockme2(3,2),
@@ -129,25 +139,25 @@ test0e_test() ->
     ok = testmodule1:mockme2(2,3),
     ok = testmodule3:mockme(777),
 
-    mock:verify(Mock).
+    ltest_mock:verify(Mock).
 
 test1_test() ->
-    Mock = mock:new(),
-    mock:replay(Mock),
-    mock:verify(Mock).
+    Mock = ltest_mock:new(),
+    ltest_mock:replay(Mock),
+    ltest_mock:verify(Mock).
 
 %error expected
 test2_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule, mockme, [1,2],{return, ok}),
-    mock:replay(Mock),
-    {error, _} = mock:verify(Mock).
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule, mockme, [1,2],{return, ok}),
+    ltest_mock:replay(Mock),
+    {error, _} = ltest_mock:verify(Mock).
 
 
 test3_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule2, mockme1, [666], {error, end_of_times}),
-    mock:replay(Mock),
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule2, mockme1, [666], {error, end_of_times}),
+    ltest_mock:replay(Mock),
     try testmodule2:mockme1(666) of
         _ ->
             exit(error_expected)
@@ -157,9 +167,9 @@ test3_test() ->
     end.
 
 test3a_test() ->
-    Mock = mock:new(),
-    mock:o_o(Mock, testmodule2, mockme1, [666], {throw, end_of_times}),
-    mock:replay(Mock),
+    Mock = ltest_mock:new(),
+    ltest_mock:o_o(Mock, testmodule2, mockme1, [666], {throw, end_of_times}),
+    ltest_mock:replay(Mock),
     try testmodule2:mockme1(666) of
         _ ->
             exit(error_expected)
@@ -169,9 +179,9 @@ test3a_test() ->
     end.
 
 test3b_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule2, mockme1, [666], {exit, end_of_times}),
-    mock:replay(Mock),
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule2, mockme1, [666], {exit, end_of_times}),
+    ltest_mock:replay(Mock),
     try testmodule2:mockme1(666) of
         _ ->
             exit(error_expected)
@@ -181,56 +191,56 @@ test3b_test() ->
     end.
 
 test4_test() ->
-    Mock = mock:new(),
-    {error,_} = mock:verify(Mock).
+    Mock = ltest_mock:new(),
+    {error,_} = ltest_mock:verify(Mock).
 
 test4a_test() ->
-    Mock = mock:new(),
+    Mock = ltest_mock:new(),
     {'EXIT',_} =
-        (catch mock:strict(
+        (catch ltest_mock:strict(
                  Mock, testmodule1, mockme1, [1,2],
                  {hier_steht_was_falsches, xxx})).
 
 test5_test() ->
-    Mock = mock:new(),
-    mock:strict(Mock, testmodule1, mockme1, [1,2], {rec_msg, self()}),
-    mock:replay(Mock),
+    Mock = ltest_mock:new(),
+    ltest_mock:strict(Mock, testmodule1, mockme1, [1,2], {rec_msg, self()}),
+    ltest_mock:replay(Mock),
     TestPid = spawn(testmodule1,mockme1,[1,2]),
     TestPid ! test,
     receive
         test ->
             ok,
-            mock:verify(Mock)
+            ltest_mock:verify(Mock)
     after 1000 ->
             error
     end.
 
 test6_test() ->
-    Mock = mock:new(),
-    mock:o_o(
+    Mock = ltest_mock:new(),
+    ltest_mock:o_o(
       Mock, testmodule1, mockme1, [1,2],
       {function, fun(X,Y) ->
                          X + Y
                  end}),
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
     R = testmodule1:mockme1(1,2),
-    mock:verify(Mock),
+    ltest_mock:verify(Mock),
     3 = R.
 
 test6a_test() ->
-    Mock = mock:new(),
-    mock:stub(
+    Mock = ltest_mock:new(),
+    ltest_mock:stub(
       Mock, testmodule1, mockme1, [1,2],
       {function, fun(_,_) ->
                          erlang:error(test)
                  end}),
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
     {'EXIT',_} = (catch testmodule1:mockme1(1,2)),
-    mock:verify(Mock).
+    ltest_mock:verify(Mock).
 
 test7_test() ->
-    Mock = mock:new(),
-    mock:expect(
+    Mock = ltest_mock:new(),
+    ltest_mock:expect(
       Mock, in_order, testmodule1, mockme1, 1,
       fun([{qXYZ, D, B, A}]) when A >= B andalso B >= D ->
               true
@@ -238,14 +248,14 @@ test7_test() ->
       {function, fun({qXYZ, D, B, A}) ->
                          [B,D|A]
                  end}),
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
     L = testmodule1:mockme1({qXYZ, 1,2,3}),
-    mock:verify(Mock),
+    ltest_mock:verify(Mock),
     [2,1|3] = L.
 
 test7a_test() ->
-    Mock = mock:new(),
-    mock:expect(
+    Mock = ltest_mock:new(),
+    ltest_mock:expect(
       Mock, in_order, testmodule1, mockme1, 1,
       % hier fehlen die obligatorischen Klammern
       fun({qXYZ, D, B, A}) when A >= B andalso B >= D ->
@@ -255,5 +265,5 @@ test7a_test() ->
        fun({qXYZ, D, B, A}) ->
                [B,D|A]
        end}),
-    mock:replay(Mock),
+    ltest_mock:replay(Mock),
     {'EXIT',_} = (catch testmodule1:mockme1({qXYZ, 1,2,3})).
